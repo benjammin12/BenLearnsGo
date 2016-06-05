dc60d287b39227ff16e0615ff694b4d6fcd4769a
benjammin12 <benxerri@gmail.com>
2016-06-05 18:09:58 -0400

    Added functions

--- Functions/Basic.go
+++ Functions/Basic.go
@@ -6,12 +6,22 @@ func main() {
 
 	fmt.Println(addUpAll(4, 3, 2, 3, 21, 22)) //takes mult values seperate with commas
 	fmt.Println(doubleMe(13))
+	fmt.Println(factorial(5))
 }
 
 func doubleMe(val int) int { // basic functions, takes first parenthesis is the parameters
 	// the return value always comes after the parameters
 
 	return val * 2
+}
+
+func factorial(val int) int {
+	if val == 0 {
+		return 1
+	}
+
+	return val * factorial(val-1)
+
 }
 
 func addUpAll(args ...int) int { // use (args ...int) if you don't know how many values you will take in
--- TwoValueFunc/TwoReturns.go
+++ TwoValueFunc/TwoReturns.go
@@ -3,10 +3,16 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println(returnTwo(4))
+	fmt.Println(returnTwo(12))
+	fmt.Println(isEvenOrOdd(14))
+
 }
 
 //functions in go can return two Values
 func returnTwo(val int) (int, int) { //simply put the return values in parenthesis and seperate by a comma
 	return val + 1, val + 2 //seperate the return by a comma, only write return once
+}
+
+func isEvenOrOdd(val int) (int, bool) {
+	return val / 2, val%2 == 0
 }
