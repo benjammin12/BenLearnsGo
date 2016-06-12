62cc5ebb176f5d44441ca0f2b46b1763895c5a5f
benjammin12 <benxerri@gmail.com>
2016-06-11 22:08:20 -0400

    More structs

--- Struct/main.go
+++ Struct/main.go
@@ -4,9 +4,9 @@ import "fmt"
 
 func main() {
 
-	rect := Rectangle{5, 6} //creating a struct or an object
-	rect.x = 10             //sets first var x to 10
-	area1 := rect.area()
+	rectObj := Rectangle{5, 6} //creating a struct or an object
+	rectObj.x = 10             //sets first var x to 10
+	area1 := rectObj.area()
 
 	fmt.Println(area1)
 
@@ -16,10 +16,14 @@ func main() {
 }
 
 //Rectangle x,y
-type Rectangle struct {
+type Rectangle struct { //create a struct with 2 variables representings sides of rect
 	x, y int
 }
 
-func (rect *Rectangle) area() int {
+func (rect Rectangle) area() int { //METHOD that calculates area
+	//Methods have recievers, in this case the reciever is rect Rectangle
+	//if you declare like func (rect *Rectangle) using a pointer instead
+	// then any modification of rect.x or rect.y inside the function will change the Rectangle
+	//object we created as well
 	return rect.x * rect.y
 }
--- Structs/main.go
+++ Structs/main.go
@@ -11,17 +11,28 @@ func main() {
 	fmt.Println("Rectange area is", getArea(rect1))
 	fmt.Println("Square area is", getArea(square1))
 
+	fmt.Println("Rectangle perimeter is", getPerimeter(rect1))
+	fmt.Println("Square perimeter is", getPerimeter(square1))
+
 }
 
+//AreaOfShape returns area
 type AreaOfShape interface { //Area interface that gets Area
 	area() int
 }
 
+//PerimeterOfShape returns perimeter
+type PerimeterOfShape interface {
+	perimeter() int
+}
+
+//Rectangle width and height
 type Rectangle struct { //Rectange structure, similiar to creating objects
 	width  int //width
 	height int //height
 }
 
+//Square one side
 type Square struct { //square struct
 	side int
 }
@@ -34,6 +45,18 @@ func (rect Rectangle) area() int { //create an area method that points to Rectan
 	return rect.width * rect.height
 }
 
+func (s Square) perimeter() int {
+
+	return s.side * 4
+}
+func (rect Rectangle) perimeter() int {
+	return 2 * (rect.width * rect.height)
+}
+
 func getArea(shape AreaOfShape) int { //use to get area of whatever shape you want
 	return shape.area()
+}
+
+func getPerimeter(sh PerimeterOfShape) int {
+	return sh.perimeter()
 }
