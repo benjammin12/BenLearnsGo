282757919dea79afeef653882a11d2d07a2e4774
benjammin12 <benxerri@gmail.com>
2016-06-16 19:23:45 -0400

    Added High Charts

---
+++ HighChart/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"encoding/csv"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type record struct {
+	Date string
+	Open float64
+}
+
+func makeRecord(row []string) record {
+	open, _ := strconv.ParseFloat(row[1], 64)
+
+	return record{
+		Date: row[0],
+		Open: open,
+	}
+}
+
+func main() {
+	r, err := os.Open("table.csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer r.Close()
+
+	read := csv.NewReader(r)
+	rows, err := read.ReadAll()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Println(`<!DOCTYPE html>
+<head>
+<script src="http://code.jquery.com/jquery-1.11.3.min.js"></script>
+<script src="http://code.highcharts.com/highcharts.js"></script>
+<script src="http://code.highcharts.com/modules/exporting.js"></script>
+</head>
+<body>
+<div id="container" style="min-width: 310px; height: 400px; margin: 0 auto"></div>
+<table>
+  <thead>
+    <tr>
+      <th>Date</th>
+      <th>Open</th>
+    </tr>
+  </thead>
+  <tbody>
+  `)
+
+	openValues := []string{}
+
+	for i, row := range rows {
+		if i == 0 {
+			continue
+		}
+
+		record := makeRecord(row)
+		fmt.Println(record)
+		fmt.Println(`
+          <tr>
+            <td>` + record.Date + `</td>
+            <td>` + fmt.Sprintf("%.2f", record.Open) + `</td>
+          </tr>
+          `)
+
+		openValues = append(openValues, fmt.Sprintf("%.2f", record.Open))
+
+	}
+
+	fmt.Println(`
+        </tbody>
+      </table>
+      <script>
+      $(function () {
+          $('#container').highcharts({
+              title: {
+                  text: 'Monthly Average Temperature',
+                  x: -20 //center
+              },
+              subtitle: {
+                  text: 'Source: WorldClimate.com',
+                  x: -20
+              },
+              xAxis: {
+                  categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+                      'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+              },
+              yAxis: {
+                  title: {
+                      text: 'Temperature (°C)'
+                  },
+                  plotLines: [{
+                      value: 0,
+                      width: 1,
+                      color: '#808080'
+                  }]
+              },
+              tooltip: {
+                  valueSuffix: '°C'
+              },
+              legend: {
+                  layout: 'vertical',
+                  align: 'right',
+                  verticalAlign: 'middle',
+                  borderWidth: 0
+              },
+              series: [{
+                  name: 'Tokyo',
+                  data: [
+    ` + strings.Join(openValues, ",") + `
+                  ]
+              }]
+          });
+      });
+      </script>
+    </body>
+        `)
+}
