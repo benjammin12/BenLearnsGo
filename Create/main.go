3b2dee8b483e944a441100b46513a8ca8c70f331
benjammin12 <benxerri@gmail.com>
2016-06-22 21:50:21 -0400

    Added optional 3rd argument

--- Create/main.go
+++ Create/main.go
@@ -6,10 +6,14 @@ import (
 )
 
 func main() {
-	f, err := os.Create(os.Args[1])
+	f, err := os.Create(os.Args[1]) //second argument
 	if err != nil {
 		log.Fatalln("Error here")
 	}
 
-	f.WriteString("Something will go here\n")
+	f.WriteString(os.Args[2]) // now takes third argument, you can pass a string
+
+	//example would be in terminal
+	//$Create something.txt "Here are some words"
+	//this will create a local file called osmething.txt with a string here are some words inside
 }
